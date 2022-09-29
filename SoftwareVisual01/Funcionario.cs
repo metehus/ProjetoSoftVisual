90e923d97d8786d97b198d2e8532e4ffb61fcb36
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2022-09-29 19:53:01 -0300

    Update Funcionario.cs

--- SoftwareVisual01/Funcionario.cs
+++ SoftwareVisual01/Funcionario.cs
@@ -11,7 +11,7 @@ namespace Salao
         public ulong id {get; set;}
         public string? cpf {get; set;}
         public string? nome {get; set;}
-        public string? numeroCelular {get; set;}
+        public string? telefone {get; set;}
         public string? email {get; set;}
     }
 }
