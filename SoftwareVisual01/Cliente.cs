532bd7557d4719ff506ba18c55a201d3bb1ff002
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2022-09-29 19:52:43 -0300

    Update Cliente.cs

--- SoftwareVisual01/Cliente.cs
+++ SoftwareVisual01/Cliente.cs
@@ -8,7 +8,6 @@ namespace Salao
     class Cliente
     {
         public long id {get; set;}
-        //public string? cpf {get; set;}
         public string? nome {get; set;}
         public string? telefone {get; set;}
         public string? email {get; set;}
