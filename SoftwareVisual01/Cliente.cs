a38b917df451d94cffd116d96aa2963338c9b068
Lugaw <83958914+LuizGabrielWojtovicz@users.noreply.github.com>
2022-09-29 19:54:25 -0300

    Update Cliente.cs

--- SoftwareVisual01/Cliente.cs
+++ SoftwareVisual01/Cliente.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Salao
